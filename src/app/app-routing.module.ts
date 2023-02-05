1b3e4451ebbd1ac66f771f4e784f4ac61955b2d0
willianac <willianac@live.com>
2023-02-04 22:35:59 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component'
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
-  {path: "user/photos", component: PhotoListComponent},
-  {path: "photos/add", component: PhotoFormComponent}
+  {path: "user/:photos", component: PhotoListComponent},
+  {path: "photos/add", component: PhotoFormComponent},
+  {path: "**", component: NotFoundComponent}
 ];
 
 @NgModule({
