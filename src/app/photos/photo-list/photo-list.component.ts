75aa077a23414e12ddd1e30659863a3a787c63c9
willianac <willianac@live.com>
2023-02-07 11:31:44 -0300

    add search bar

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -11,14 +11,26 @@ interface IimagemProps {
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
-  photos: IimagemProps[] = []
+export class PhotoListComponent implements OnInit  {
+  photos: IimagemProps[] = [];
+  values:string = "";
+  filteredPhotos: IimagemProps[] = [];
 
   constructor(private photoService: PhotoService) { }
 
+  filter(event: KeyboardEvent) {
+    let letter = (event.target as HTMLInputElement).value
+    if(letter) {
+      this.filteredPhotos = this.photos.filter(photo => photo.alternativo.toLowerCase().includes(letter)) 
+    }
+  }
+  
   ngOnInit(): void {
     this.photoService
     .listPhoto()
-    .subscribe(photo => this.photos = photo)
+    .subscribe(photo => {
+      this.photos = photo
+      this.filteredPhotos = photo
+    })
   }
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http"
 import { Injectable } from "@angular/core"
 
-interface IimagemProps {
+export interface IimagemProps {
     url: string
     alternativo: string
 }
