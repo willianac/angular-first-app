e09990f7d3bba3000a2a719fdc6177c875e0d73f
willianac <willianac@live.com>
2023-02-27 14:10:55 -0300

    add delete and like methods

--- src/app/carros/carros.service.ts
+++ src/app/carros/carros.service.ts
@@ -1,12 +1,14 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { environment } from 'src/environments/environment.development';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
 import { TokenService } from '../autenticacao/token.service';
 import { ICarro } from './carros';
 
 const API = environment.apiURL
+const NOT_MODIFIED = "304"
+
 @Injectable({
   providedIn: 'root'
 })
@@ -20,5 +22,19 @@ export class CarrosService {
 
   public buscaPorId(id:number): Observable<ICarro> {
     return this.http.get<ICarro>(`${API}/photos/${id}`)
+  }
+
+  public excluiCarro(id:number): Observable<ICarro> {
+    return this.http.delete<ICarro>(`${API}/photos/${id}`)
+  }
+
+  public curtir(id: number): Observable<boolean> {
+    return this.http.post(`${API}/photos/${id}/like`, {}, {observe : "response"})
+      .pipe(
+        map(() => true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(() => new Error(error))
+        })
+      )
   }
 }
